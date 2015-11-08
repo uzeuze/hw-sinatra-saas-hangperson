e6bdb4141ac72eed6e4dcb7f08b3589d6a319b18
Egemen Can Uze <egemencanuze@gmail.com>
2015-11-08 11:08:52 +0000

    Removed Hello World!

--- app.rb
+++ app.rb
@@ -64,9 +64,3 @@ class HangpersonApp < Sinatra::Base
   end
   
 end
-
-class MyApp < Sinatra::Base
-  get '/' do
-    "<!DOCTYPE html><html><head></head><body><h1>Hello World!</h1></body></html>"
-  end
-end
