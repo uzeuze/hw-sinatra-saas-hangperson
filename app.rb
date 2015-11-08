0854d0fa1405ba141fde25d6cf873b4e01cf4ab8
Egemen Can Uze <egemencanuze@gmail.com>
2015-11-08 10:56:20 +0000

    Added MyApp

--- app.rb
+++ app.rb
@@ -64,3 +64,9 @@ class HangpersonApp < Sinatra::Base
   end
   
 end
+
+class MyApp < Sinatra::Base
+  get '/' do
+    "<!DOCTYPE html><html><head></head><body><h1>Hello World!</h1></body></html>"
+  end
+end
